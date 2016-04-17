c41468dd908a3fb4c51fd78a418a72289e8401d9
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2016-04-17 00:21:48 -0400

    This is version 0.0.11

--- src/js/interface.ts
+++ src/js/interface.ts
@@ -126,7 +126,7 @@ dc.useRemoteData = true;
  * Singular class
  */
 class Singular {
-    public version:string = '0.0.10';
+    public version:string = '0.0.11';
     public items:any[] = [];//any chart type
 
     /**
